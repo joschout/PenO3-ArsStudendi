ded62dbbd93710de0feb9192ae83290aa412b544
michiel.meertens1@gmail.com <michiel.meertens1@gmail.com@1f821998-72bf-5379-82d2-3c3a78102adf>
2012-11-23 14:10:54 +0000

    milestone

--- ArsStudendi/src/DomainModel/Milestone.java
+++ ArsStudendi/src/DomainModel/Milestone.java
@@ -3,31 +3,52 @@ package DomainModel;
 public class Milestone {
 
 	private String name;
-	private Activity activity;
+	private Calender startTime;
+	private Calender stopTime;
 	private boolean isAchieved;
+	private Course course;
 	private MilestoneType milestoneType;
+	//goal = aantal bladzijden/aantal seconden men wilt leren
+	private int goal;
+	private int  progress;
 	
-	public Milestone(String name, Activity activity){
-		this.name = name;
-		this.activity = activity;
-	}
-public String getMilestoneName(){
-	return name;
-}
-public MilestoneType milestoneType(){
-	return milestoneType;
-}
-public boolean isAchieved(){
-	return isAchieved;
-}
-public void setMilestoneName(String name){
-	this.name=name;
-}
-public Activity getActivity(){
-	return activity;
-}
-public void setActivity(Activity activity){
-this.activity = activity;
 
-}
+	public Milestone(String name, Course course, Milestonetype milestoneType,int goal) {
+		this.startTime=startTime;
+		this.stopTime = stopTime;
+		this.name = name;
+		this.goal = goal;
+		this.milestoneType = milestoneType;
+	}
+	public void setStartTime(Calender startTime)
+	{
+	this.startTime = startTime;	
+	}
+	public void setStopTime(Calender stopTime){
+		this.stopTime=stopTime;
+	}
+	public String getMilestoneName() {
+		return name;
+	}
+
+	public MilestoneType milestoneType() {
+		return milestoneType;
+	}
+
+	public boolean isAchieved() {
+		return isAchieved;
+	}
+
+	public void setMilestoneName(String name) {
+		this.name = name;
+	}
+
+	public Activity getActivity() {
+		return activity;
+	}
+
+	public void setActivity(Activity activity) {
+		this.activity = activity;
+
+	}
 }
